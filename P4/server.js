24b33687457515ca0f5d4e5e1f529ad9cba3ceec
20156865 AXEL RAMON GUERRERO DIAZ <20156865@TELEMATICA.UCOL.MX>
2022-03-03 12:02:33 -0600

    P4: Vista con .pug

--- P4/server.js
+++ P4/server.js
@@ -1,24 +1,26 @@
 require('dotenv').config()
+
 const express = require('express')
 const app = express()
-const { appConfig } = require('./lib/config')
 const bodyParser = require('body-parser')
+const md5 = require('md5')
+const { appConfig } = require('./lib/config')
+
 app.use(bodyParser.urlencoded({ extended: false }))
-
 app.set('view engine', 'pug');
-
 
 app.get('/', function(req, res) {
     res.render('index');
 });
+
 app.get('/practica', function(req, res) {
-    //Renderizar vista
-    res.render('vista');
-});
-app.post('/ingreso', function(req, res) {
-    let pass = md5(req.body.oass);
-    res.render('ingreso',{correo:req.body.email, pass:pass});
-    //res.send(`Has ingresado con el correo ${req.body.email} y con la contraseña ${req.body.pass}`)
+    res.render('practica');
 });
 
-app.listen(appConfig.port, ()=> console.log(`Puesto en marcha en puerto ${appConfig.port}`))
+app.post('/ingreso', function(req, res) {
+    //res.send(`Has ingresado con el correo ${req.body.email} y con la contraseña ${req.body.pass}`)
+    let pass = md5(req.body.pass)
+    res.render('ingreso', {correo: req.body.email, pass: pass});
+});
+
+app.listen(appConfig.port, ()=> console.log(`Puesto en marcha en puerto ${appConfig.port}`)) 
