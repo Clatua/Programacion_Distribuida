2dea972602a8ad7da293541cbed0629dff9fa945
clatua <aguerrero10@ucol.mx>
2022-03-02 19:59:32 -0600

    P3: NodeJS, ExpressJS y MySQL

---
+++ P3/db/mysql.js
@@ -0,0 +1,17 @@
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : '123456789',
+    database : 'telematica4c'
+});
+
+connection.connect((err)=>{
+    if(err){
+        console.log(err);
+        return;
+    }else{
+        console.log('DB is connected');
+    }
+});
+module.exports=connection;
---
+++ P3/readme.md
@@ -0,0 +1,14 @@
+# P3: NodeJS, ExpressJS y MySQL
+#### Tercera practica de programacion distribuida de servicios de internet
+
+Guerrero Diaz Axel Ramon
+
+    Se usaron las dependencias:
+
+- express
+
+- nodemon
+
+- dotenv
+
+- mysql
